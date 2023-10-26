c0b0153a8dd4ba96927dabedddc25b456a24bdab
stefin <stefinracho@gmail.com>
2023-10-25 19:32:04 -0700

    first night

---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Card.css";
+
+export default function Card() {
+  return (
+    <div className="card">
+      <img
+        // src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?w=2000"
+        alt="test-image"
+        className="image"
+      />
+    </div>
+  );
+}
