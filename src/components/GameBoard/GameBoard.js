9117d7741be4103cbb939007572c43b6f19b22f9
stefin <stefinracho@gmail.com>
2023-10-30 19:23:21 -0700

    2 days of work

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -2,14 +2,21 @@ import React from "react";
 import Card from "../Card/Card";
 import "./GameBoard.css";
 
-export default function GameBoard() {
+export default function GameBoard({ images, hideImages }) {
+  // Shuffle the images
+  const shuffledImages = images.sort(() => Math.random() - 0.5);
+
   return (
     <div className="game-board">
-      {Array(6)
-        .fill(null)
-        .map((_, index) => (
-          <Card key={index} />
-        ))}
+      {[0, 1, 2, 3, 4, 5].map((index) => (
+        <Card
+          key={index}
+          imageUrl={
+            shuffledImages[index] ? shuffledImages[index].urls.regular : null
+          }
+          hideImages={hideImages}
+        />
+      ))}
     </div>
   );
 }
